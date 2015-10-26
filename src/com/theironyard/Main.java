9dd26acdb34a164e06fd573b74ca59e928601480
landonkail <landon.kail@gmail.com>
2015-10-26 12:22:47 -0400

    initial commit

---
+++ src/com/theironyard/Main.java
@@ -0,0 +1,38 @@
+package com.theironyard;
+
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList<>();
+        Spark.staticFileLocation("/public");
+        Spark.init();
+        Spark.post(
+                "/create-account",
+                ((request, response) -> {
+                    User user = new User();
+                    user.name = request.queryParams("username");
+                    user.password = request.queryParams("password");
+                    users.add(user);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.get(
+                "/accounts",
+                ((request, response) -> {
+                    HashMap m = new HashMap();
+                    m.put("count", users.size());
+                    m.put("accounts", users);
+                    return new ModelAndView(m, "accounts.html");
+                }),
+                new MustacheTemplateEngine()
+        );
+    }
+}
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+/**
+ * Created by landonkail on 10/26/15.
+ */
+public class User {
+    String name;
+    String password;
+}
